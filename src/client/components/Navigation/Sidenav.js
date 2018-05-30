53248a7f9e8b8ea457aba1f592c6dbaf24f4e9be
lopu <ireply@myleisure.com.au>
2018-05-30 15:15:39 +1000

    ready for prod

--- src/client/components/Navigation/Sidenav.js
+++ src/client/components/Navigation/Sidenav.js
@@ -14,19 +14,19 @@ const Sidenav = ({ username }) => (
       <ul className="Sidenav">
         <li>
           <NavLink to={`/`} activeClassName="Sidenav__item--active" exact>
-            <i className="iconfont icon-home" />
+            {/* <i className="iconfont icon-home" /> */}
             <FormattedMessage id="home" defaultMessage="Home" />
           </NavLink>
         </li>
         <li>
           <NavLink to={`/boards`}>
-            <i className="iconfont icon-home" />
+            {/* <i className="iconfont icon-home" /> */}
             <FormattedMessage id="boards" defaultMessage="Boards" />
           </NavLink>
         </li>
         <li>
           <NavLink to={`/feeds`}>
-            <i className="iconfont icon-home" />
+            {/* <i className="iconfont icon-home" /> */}
             <FormattedMessage id="feeds" defaultMessage="Feeds" />
           </NavLink>
         </li>
@@ -38,50 +38,50 @@ const Sidenav = ({ username }) => (
 				</li>
         <li>
           <NavLink to={`/groups`}>
-            <i className="iconfont icon-home" />
+            {/* <i className="iconfont icon-home" /> */}
             <FormattedMessage id="groups" defaultMessage="Groups" />
           </NavLink>
         </li>
         <li>
           <NavLink to={`/events`}>
-            <i className="iconfont icon-home" />
+            {/* <i className="iconfont icon-home" /> */}
             <FormattedMessage id="events" defaultMessage="Events" />
           </NavLink>
         </li>
         <li>
 					<NavLink to="/wallet" activeClassName="Sidenav__item--active" isActive={isWallet}>
-            <i className="iconfont icon-home" />
+            {/* <i className="iconfont icon-home" /> */}
             <FormattedMessage id="wallet" defaultMessage="Wallet" />
           </NavLink>
         </li>
         <li>
           <NavLink to={`/exchange`}>
-            <i className="iconfont icon-home" />
+            {/* <i className="iconfont icon-home" /> */}
             <FormattedMessage id="exchange" defaultMessage="Exchange" />
           </NavLink>
         </li>
         <li>
           <NavLink to={`/marketplace`}>
-            <i className="iconfont icon-home" />
+            {/* <i className="iconfont icon-home" /> */}
             <FormattedMessage id="market_place" defaultMessage="Market Place" />
           </NavLink>
         </li>
         <li>
           <NavLink to={`/applications`}>
-            <i className="iconfont icon-home" />
+            {/* <i className="iconfont icon-home" /> */}
             <FormattedMessage id="applications" defaultMessage="Applications" />
           </NavLink>
         </li>
         <li>
           <NavLink to={`/network`}>
-            <i className="iconfont icon-home" />
+            {/* <i className="iconfont icon-home" /> */}
             <FormattedMessage id="network" defaultMessage="Network" />
           </NavLink>
         </li>
         {username && (
 					<li>
 						<NavLink to={`/saved`}>
-							<i className="iconfont icon-home" />
+							{/* <i className="iconfont icon-home" /> */}
 							<FormattedMessage id="saved" defaultMessage="Saved" />
 						</NavLink>
 					</li>
@@ -89,7 +89,7 @@ const Sidenav = ({ username }) => (
 				{username && (
 					<li>
 						<NavLink to={`/music`}>
-							<i className="iconfont icon-home" />
+							{/* <i className="iconfont icon-home" /> */}
 							<FormattedMessage id="music" defaultMessage="Music" />
 						</NavLink>
 					</li>
